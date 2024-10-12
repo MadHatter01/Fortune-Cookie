55302906728d07fd0acb42804f15a276a57dfed5
Vaishnavi Rangarajan <r.vaishnavi15@gmail.com>
2024-10-12 16:35:43 -0700

    basic fortune telling

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
+const fortunes = {
+  'funny':[
+    'Life is what happens to you while you are busy making other plans.',
+    'Borrow money from a pessimist. They don\'t expect it back.',
+    'Beware of funny seagulls',
+    'In a past life, you were a tomato',
+    'You will soon invent a new kind of salad',
+    'Careful! You might step on gum today'
+  ],
+
+  'inspirational':[
+    'The sky is the limit. You go for it!',
+    'Whatever you are waiting to start, do it today',
+    'Do one item from your bucket list today!',
+    'You are capable of amazing things',
+    'You need to believe in yourself!'
+  ]
+}
+
+const keys = ['funny', 'inspirational']
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [fortune, setFortune] = useState('');
+
+  const getFortune = ()=>{
+    const randIndex = Math.floor(Math.random()* keys.length)
+    // const randIndex = Math.floor(Math.random()*Object.keys(fortunes).length);
+    setFortune(fortunes[keys[randIndex]][0])
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className='cookie' onClick={getFortune}> 🥠</div> 
+      <p>{fortune}</p>
     </>
   )
 }
